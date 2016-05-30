e0dddc6e9c787edf35f95cdf96366d2d86c7a575
ninedreams <657462308@qq.com>
2016-05-30 10:07:39 +0800

    第一个Fragment为特色，第二个更改为社区，导航栏一些更改

---
+++ app/src/main/java/cn/panorama/slook/ui/fragment/PlaceFragment.java
@@ -0,0 +1,7 @@
+package cn.panorama.slook.ui.fragment;
+
+/**
+ * Created by xingyaoma on 16-5-30.
+ */
+public class PlaceFragment {
+}
---
+++ app/src/main/java/cn/panorama/slook/utils/TabLayoutPlace.java
@@ -0,0 +1,7 @@
+package cn.panorama.slook.utils;
+
+/**
+ * Created by xingyaoma on 16-5-30.
+ */
+public class TabLayoutPlace {
+}
